c177bb71a55af7827b4c4b5e5a5aa5f4fa08aa80
arunprakashpv <arunprakashvellingiri@gmail.com>
2022-07-08 22:44:55 +0530

    push test

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 aa1 = 'Arun'
 aa2 = ' is interested'
 aa3 = ' developing'
+aa4 = 'programs'
+bb1= 'through this technologies'
 
 
-
-interest = aa1+aa2+aa3
+interest = aa1+aa2+aa3+aa4+bb1
 
 print(interest)
  
